0cbb1fa11e5fc9d3153d3209bef8470d8ec8c9bb
Khanh Nghiem <knghiem@conncoll.edu>
2017-10-29 04:17:37 -0400

    check wellness

--- add_post.php
+++ add_post.php
@@ -8,12 +8,13 @@
 ?>
 
 <?php
-    if( !empty($_POST["title"]) && !empty($_POST["point"])){
+    if(!empty($_POST["title"])) {
         $title = $_POST["title"];
         $des = $_POST["des"];
         $point = $_POST["point"];
+				$wellness = $_POST["wellness"];
 
-        $username = $_SESSION['active'];
+        $email = $_SESSION['active'];
 
         // OPEN AND SELECT DATABASE
         $db_conn = mysqli_connect("localhost", "root", "");
@@ -21,8 +22,7 @@
           die("Unable to connect: " . mysqli_connect_error());
         mysqli_select_db($db_conn, "gilit_db");
 
-        $cmd  = "SELECT * from users where username='".$username."'";
-
+        $cmd  = "SELECT * from users where email='".$email."'";
         $result=mysqli_query($db_conn, $cmd);
 
         if (mysqli_num_rows($result)==1){
@@ -31,24 +31,23 @@
                 $com_id = $row["com_id"];
 								$user_point = $row['point'];
             }
-						if ($user_point < $point)
+						if ($user_point < $point && $wellness!=true)
 							echo "Not enough points";
 						else{
-		            $cmd2 = "INSERT INTO posts (com_id, title, des, get_id, point) VALUES
+		            $cmd2 = "INSERT INTO posts (com_id, title, des, getter_id, post_point) VALUES
 		                (".$com_id.", '".$title."', '".$des."', ".$user_id.", ".$point.")
 		                ;";
 
 		            if( mysqli_query($db_conn, $cmd2) )
 		                echo "Your post have been added to the board";
 		            else
-		                echo "Attempted to add post but failed";
+		                echo mysqli_error($db_conn);
 									}
-								}
-        else{
-            echo "Database error".PHP_EOL;
-        }
-    }
+				}
+        else
+            echo mysqli_error($db_conn);
 
+    }
     else
         echo "Title and Point are required fields";
 
